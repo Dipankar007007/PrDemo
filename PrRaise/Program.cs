452391ed3d4131d81e620ecfe2aacd6c40449653
Dipankar007007 <spritam0305@gmail.com>
2022-10-21 10:52:37 +0530

    First commit

---
+++ PrRaise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrRaise
+{
+    internal class Program
+    {
+        public void Test1()
+        {
+            Console.WriteLine("Test1 Method");
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
