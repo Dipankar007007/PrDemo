103958887046da7851f91c11c0dfad52e3fb2e16
Dipankar007007 <spritam0305@gmail.com>
2022-10-21 11:06:03 +0530

    Test2 method added

--- PrRaise/Program.cs
+++ PrRaise/Program.cs
@@ -8,6 +8,11 @@ namespace PrRaise
         {
             Console.WriteLine("Test1 Method");
         }
+
+        public void Test2()
+        {
+            Console.WriteLine("Test2 Method");
+        }
         static void Main(string[] args)
         {
         }
